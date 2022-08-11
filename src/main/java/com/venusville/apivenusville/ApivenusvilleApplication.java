02cd54f3a166b249228e1aee9601e3f60d34f4fd
Leandro Lima <leo.amaru85@gmail.com>
2022-08-11 18:09:28 -0300

    Subindo arquivos inicias do Spring Boot

---
+++ src/main/java/com/venusville/apivenusville/ApivenusvilleApplication.java
@@ -0,0 +1,13 @@
+package com.venusville.apivenusville;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApivenusvilleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApivenusvilleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/venusville/apivenusville/ApivenusvilleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venusville.apivenusville;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApivenusvilleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
